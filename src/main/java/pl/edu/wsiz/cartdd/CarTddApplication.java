537b23389e3c3c5eac930503f6e117e0f454cfcc
Piotr Hendzel <pihe@jcommerce.pl>
2021-03-21 10:15:21 +0100

    Add an initial version of the project

---
+++ src/main/java/pl/edu/wsiz/cartdd/CarTddApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.wsiz.cartdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarTddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarTddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/edu/wsiz/cartdd/CarTddApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.wsiz.cartdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarTddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
